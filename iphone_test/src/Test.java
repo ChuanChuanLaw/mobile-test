c23231422676d1e688b6270a4fcfa14b5f3e86a2
ChuanChuanLaw <chuan.chuan247@gmail.com>
2012-09-24 11:15:11 +1000

    Complete tests for mobile tests

--- iphone_test/src/Test.java
+++ iphone_test/src/Test.java
@@ -14,13 +14,17 @@ import org.junit.*;
 public class Test
 {
    public static void main ( String[] args ) throws Exception
-   {
+   {   
+	   //Initialize the webdriver. It should be smart enough to detect that it is a mobile app
 	   WebDriver driver = new RemoteWebDriver(new URL("http://10.12.2.18:3001/wd/hub"), DesiredCapabilities.iphone());
 	   driver.get("http://careers.pageuppeople.com/218/caw/en/listing");
 	   
+	   
+	   //Search by keyword
 	   WebElement search = driver.findElement(By.name("search-keyword"));
 	   search.sendKeys("test analyst");
 	   
+	   //Enter the location. This is not a filter.
 	   WebElement location = driver.findElement(By.name("search-location"));
 	   location.sendKeys("Melbourne, VIC");
 	   
@@ -29,34 +33,57 @@ public class Test
 	   locationLink.click();
 	   
 	   search.submit();
-	    
+	   
+	   //Looking for search keyword returned
 	   boolean searchResult=driver.getPageSource().contains("test"); 
 	   Assert.assertEquals(searchResult, true);
 	   System.out.println("Job found");
 	   
-	   driver.findElement(By.className("job-link")).click();
-	   
+	   //Click into the job details
+	   driver.findElement(By.className("job-link")).click();	   
 	   boolean jobNo=driver.getPageSource().contains("Job no:"); 
 	   Assert.assertEquals(jobNo, true);
 	   System.out.println("Job no found");
 	   
-	   WebElement email=driver.findElement(By.name("mobile-apply-email"));
-	   email.sendKeys("chuanl@pageuppeople.com");
+	   //Grab the job title
+	   String jobTitle1=driver.findElement(By.xpath("//span[@id='page-heading']")).getText();
+	   System.out.println(jobTitle1);
 	   
+	   //Apply for the job
+	   WebElement email=driver.findElement(By.name("mobile-apply-email"));
+	   email.sendKeys("chuanl@pageuppeople.com");	   
 	   driver.findElement(By.xpath("//input[@value='Send']")).click();
 	   
 	   boolean msg=driver.getPageSource().contains("Thank you! You will receive an email");
 	   Assert.assertEquals(msg, true);
 	   System.out.println("Email sent");
 	   
-	   driver.findElement(By.linkText("Back to list")).click();
+	   //Test integration with Facebook
+	   driver.findElement(By.linkText("Facebook")).click();
+	   WebElement fbEmail = wait.until(ExpectedConditions.elementToBeClickable(By.name("email")));
+	   fbEmail.sendKeys("qa@pageuppeople.com");
 	   
+	   WebElement fbPass = driver.findElement(By.name("pass"));
+	   fbPass.sendKeys("pagetestup");
+	   
+	   driver.findElement(By.name("login")).click();
+	   
+	   boolean fbMsg=driver.getPageSource().contains("Post a link to your profile");
+	   Assert.assertEquals(fbMsg, true);
+	   System.out.println("Facebook message");
+	   
+	   //Navigate back to mobile site
+	   driver.navigate().back();
+	   driver.navigate().back();
+	   
+	   //Back to search results
+	   driver.findElement(By.linkText("Back to list")).click();	   
 	   boolean backBtn = driver.getPageSource().contains("Search Results");
 	   Assert.assertEquals(backBtn, true);
 	   System.out.println("Back to search results");
 	   
-	   driver.findElement(By.linkText("Send me jobs like these")).click();
-	   
+	   //Send me more jobs like this
+	   driver.findElement(By.linkText("Send me jobs like these")).click();	   
 	   WebElement subscription = driver.findElement(By.name("job-mail-subscribe-email"));
 	   subscription.sendKeys("test@test.com");
 	   
@@ -65,6 +92,20 @@ public class Test
 	   boolean similarJobsMsg = driver.getPageSource().contains("Ok, we will send you jobs like this");
 	   Assert.assertEquals(similarJobsMsg, true);
 	   System.out.println("Similar jobs email");
+	   
+	   //More jobs
+	   driver.findElement(By.xpath("//a[@title='More Jobs']")).click();
+	   driver.findElement(By.className("job-link")).click();	 
+	   
+	   //Check that the job returned is different
+	   String jobTitle2=driver.findElement(By.xpath("//span[@id='page-heading']")).getText();
+	   System.out.println(jobTitle2);
+	   
+	   //Making sure that jobs returned are different
+	   boolean jobReturned=jobTitle1.equals(jobTitle2);
+	   Assert.assertEquals(jobReturned, false);
+	   
+	   System.out.println("TEST ALL PASSED");
 	   
 	   driver.quit();
    }
